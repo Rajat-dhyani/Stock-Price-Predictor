57179c51cd97431acfd98a27a08da2406703574e
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-07-20 03:38:57 +0530

    Added helper file for spliting the data in train and test sets

---
+++ stock_data.py
@@ -0,0 +1,75 @@
+import numpy as np
+import math
+
+
+def scale_range(x, input_range, target_range):
+    """
+
+    Rescale a numpy array from input to target range
+    :param x: data to scale
+    :param input_range: optional input range for data: default 0.0:1.0
+    :param target_range: optional target range for data: default 0.0:1.0
+    :return: rescaled array, incoming range [min,max]
+    """
+
+    range = [np.amin(x), np.amax(x)]
+    x_std = (x - input_range[0]) / (1.0*(input_range[1] - input_range[0]))
+    x_scaled = x_std * (1.0*(target_range[1] - target_range[0])) + target_range[0]
+    return x_scaled, range
+
+
+def train_test_split_linear_regression(stocks):
+    # Create numpy arrays for features and targets
+    feature = []
+    label = []
+
+    # Convert dataframe columns to numpy arrays for scikit learn
+    for index, row in stocks.iterrows():
+        # print([np.array(row['Item'])])
+        feature.append([(row['Item'])])
+        label.append([(row['Close'])])
+
+    # Regularize the feature and target arrays and store min/max of input data for rescaling later
+    feature_bounds = [min(feature), max(feature)]
+    feature_bounds = [feature_bounds[0][0], feature_bounds[1][0]]
+    label_bounds = [min(label), max(label)]
+    label_bounds = [label_bounds[0][0], label_bounds[1][0]]
+
+    feature_scaled, feature_range = scale_range(np.array(feature), input_range=feature_bounds, target_range=[-1.0, 1.0])
+    label_scaled, label_range = scale_range(np.array(label), input_range=label_bounds, target_range=[-1.0, 1.0])
+
+    # Define Test/Train Split 80/20
+    split = .315
+    split = int(math.floor(len(stocks['Item']) * split))
+
+    # Set up training and test sets
+    X_train = feature_scaled[:-split]
+    X_test = feature_scaled[-split:]
+
+    y_train = label_scaled[:-split]
+    y_test = label_scaled[-split:]
+
+    return X_train, X_test, y_train, y_test, label_range
+
+
+def train_test_split_lstm(stocks, prediction_time=1, test_data_size=450, unroll_length=50):
+    # training data
+    test_data_cut = test_data_size + unroll_length + 1
+
+    x_train = stocks[0:-prediction_time - test_data_cut].as_matrix()
+    y_train = stocks[prediction_time:-test_data_cut]['Close'].as_matrix()
+
+    # test data
+    x_test = stocks[0 - test_data_cut:-prediction_time].as_matrix()
+    y_test = stocks[prediction_time - test_data_cut:]['Close'].as_matrix()
+
+    return x_train, x_test, y_train, y_test
+
+
+def unroll(data, sequence_length=24):
+    result = []
+    for index in range(len(data) - sequence_length):
+        result.append(data[index: index + sequence_length])
+    return np.asarray(result)
+
+
