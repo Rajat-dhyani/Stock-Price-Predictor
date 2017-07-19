4b338c6742dbda7c0d162b554ac63c6b1c17b705
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-07-20 03:37:55 +0530

    Added helper file for visualization of data

---
+++ visualize.py
@@ -0,0 +1,91 @@
+#%matplotlib notebook
+#%matplotlib inline
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (18, 12)
+
+
+# format the coords message box
+def price(x):
+    return '$%1.2f' % x
+
+
+def plot_basic(stocks, title='Google Trading', y_label='Price USD', x_label='Trading Days'):
+    """
+    Plots basic pyplot
+    :param stocks: DataFrame having all the necessary data
+    :param title:  Title of the plot 
+    :param y_label: yLabel of the plot
+    :param x_label: xLabel of the plot
+    :return: prints a Pyplot againts items and their closing value
+    """
+    fig, ax = plt.subplots()
+    ax.plot(stocks['Item'], stocks['Close'], '#0A7388')
+
+    ax.format_ydata = price
+    ax.set_title(title)
+
+    # Add labels
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+
+    plt.show()
+
+
+def plot_prediction(actual, prediction, title='Google Trading vs Prediction', y_label='Price USD', x_label='Trading Days'):
+    """
+    Plots train, test and prediction
+    :param actual: DataFrame containing actual data
+    :param prediction: DataFrame containing predicted values
+    :param title:  Title of the plot
+    :param y_label: yLabel of the plot
+    :param x_label: xLabel of the plot
+    :return: prints a Pyplot againts items and their closing value
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Add labels
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+
+    # Plot actual and predicted close values
+
+    plt.plot(actual, '#00FF00', label='Adjusted Close')
+    plt.plot(prediction, '#0000FF', label='Predicted Close')
+
+    # Set title
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+
+    plt.show()
+
+
+def plot_lstm_prediction(actual, prediction, title='Google Trading vs Prediction', y_label='Price USD', x_label='Trading Days'):
+    """
+    Plots train, test and prediction
+    :param actual: DataFrame containing actual data
+    :param prediction: DataFrame containing predicted values
+    :param title:  Title of the plot
+    :param y_label: yLabel of the plot
+    :param x_label: xLabel of the plot
+    :return: prints a Pyplot againts items and their closing value
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # Add labels
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+
+    # Plot actual and predicted close values
+
+    plt.plot(actual, '#00FF00', label='Adjusted Close')
+    plt.plot(prediction, '#0000FF', label='Predicted Close')
+
+    # Set title
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+
+
+    plt.show()
+
