5a156f3d3193505dc8b9c677b332e0a2410ba501
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-07-20 03:48:44 +0530

    Added helper file containg function related to LSTM model

---
+++ lstm.py
@@ -0,0 +1,45 @@
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+
+
+def build_improved_model(input_dim, output_dim, return_sequences):
+    model = Sequential()
+    model.add(LSTM(
+        input_shape=(None, input_dim),
+        units=output_dim,
+        return_sequences=return_sequences))
+
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
+        128,
+        return_sequences=False))
+
+    model.add(Dropout(0.2))
+
+    model.add(Dense(
+        units=1))
+    model.add(Activation('linear'))
+
+    return model
+
+
+def build_basic_model(input_dim, output_dim, return_sequences):
+    model = Sequential()
+    model.add(LSTM(
+        input_shape=(None, input_dim),
+        units=output_dim,
+        return_sequences=return_sequences))
+
+    model.add(LSTM(
+        100,
+        return_sequences=False))
+
+    model.add(Dense(
+        units=1))
+    model.add(Activation('linear'))
+
+    return model
+
+
