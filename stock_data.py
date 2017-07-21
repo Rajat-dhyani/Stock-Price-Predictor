6422476791848850a2bb8ce9162f34a590f018ad
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-07-22 00:40:20 +0530

    Added proper comments for redability of code

--- stock_data.py
+++ stock_data.py
@@ -19,6 +19,15 @@ def scale_range(x, input_range, target_range):
 
 
 def train_test_split_linear_regression(stocks):
+    """
+        Split the data set into training and testing feature for Linear Regression Model
+        :param stocks: whole data set containing ['Open','Close','Volume'] features
+        :return: X_train : training sets of feature
+        :return: X_test : test sets of feature
+        :return: y_train: training sets of label
+        :return: y_test: test sets of label
+        :return: label_range: scaled range of label used in predicting price,
+    """
     # Create numpy arrays for features and targets
     feature = []
     label = []
@@ -53,6 +62,17 @@ def train_test_split_linear_regression(stocks):
 
 
 def train_test_split_lstm(stocks, prediction_time=1, test_data_size=450, unroll_length=50):
+    """
+        Split the data set into training and testing feature for Long Short Term Memory Model
+        :param stocks: whole data set containing ['Open','Close','Volume'] features
+        :param prediction_time: no of days
+        :param test_data_size: size of test data to be used
+        :param unroll_length: how long a window should be used for train test split
+        :return: X_train : training sets of feature
+        :return: X_test : test sets of feature
+        :return: y_train: training sets of label
+        :return: y_test: test sets of label
+    """
     # training data
     test_data_cut = test_data_size + unroll_length + 1
 
@@ -67,6 +87,12 @@ def train_test_split_lstm(stocks, prediction_time=1, test_data_size=450, unroll_
 
 
 def unroll(data, sequence_length=24):
+    """
+    use different windows for testing and training to stop from leak of information in the data
+    :param data: data set to be used for unrolling
+    :param sequence_length: window length
+    :return: data sets with different window.
+    """
     result = []
     for index in range(len(data) - sequence_length):
         result.append(data[index: index + sequence_length])
