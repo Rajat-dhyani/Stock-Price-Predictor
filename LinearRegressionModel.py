6ecec5cfcbf9d3794b06586b9b6076e85b600aac
Rajat Dhyani <contact@rajatdhyani.xyz>
2017-07-22 00:39:21 +0530

    Added proper comments for redability of code

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -4,6 +4,12 @@ import stock_data as sd
 
 
 def build_model(X, y):
+    """
+    build a linear regression model using sklearn.linear_model
+    :param X: Feature dataset
+    :param y: label dataset
+    :return: a linear regression model
+    """
     linear_mod = linear_model.LinearRegression()  # defining the linear regression model
     X = np.reshape(X, (X.shape[0], 1))
     y = np.reshape(y, (y.shape[0], 1))
@@ -13,6 +19,13 @@ def build_model(X, y):
 
 
 def predict_prices(model, x, label_range):
+    """
+    Predict the label for given test sets
+    :param model: a linear regression model
+    :param x: testing features
+    :param label_range: normalised range of label data
+    :return: predicted labels for given features
+    """
     x = np.reshape(x, (x.shape[0], 1))
     predicted_price = model.predict(x)
     predictions_rescaled, re_range = sd.scale_range(predicted_price, input_range=[-1.0, 1.0], target_range=label_range)
